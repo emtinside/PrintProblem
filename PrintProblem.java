2d3de68d35e7d8ae90a5fbef39721251b03e30e7
Emily Tumacder <EmilyTumacder@Emilys-MBP.attlocal.net>
2021-08-09 20:20:17 -0700

    Update PrintProblem.java

--- PrintProblem.java
+++ PrintProblem.java
@@ -1,24 +1,48 @@
+import java.util.Scanner;
+
 public class PrintProblem {
 
 	public static void main(String[] args) {
-		System.out.println(print(5,'+'));
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter # of rows: ");
+		int input = s.nextInt();
+		System.out.println(print(input,'+'));
 	}
 	
 	public static String print(int rows, char c) {
 		String s="";
 		int width=2*rows -1;
+		int temprows= rows;
 		
-	while(rows>0) {
-		for (int i=0; i<width;i++) {
-			for(int a=0; a<rows-1;a++) {
+	while(temprows>0) {
+		String nextRow = halfRow(temprows, c);
+		s+=nextRow;
+		if (temprows != rows && temprows != 1) {
+			int remain = width- 2*nextRow.length();
+			for (int i=0; i<remain;i++) {
 				s+=" ";
 			}
 			s+=c;
 		}
-		rows--;
+		if (temprows==1) {
+			for (int a=0; a<width-1; a++) {
+				s+=c;
+			}
+		}
+	
+		temprows--;
 		s+="\n";
 	}
 		
+		return s;
+	}
+	
+	private static String halfRow(int rows, char c) {
+		String s="";
+		for(int a=0; a<rows-1;a++) {
+			s+=" ";
+		}
+		s+=c;
 		return s;
 	}
 }
